0c380c3920fe79a7a9ee57424d005fe729d5304c
JiHui Luo <zhiyuanluo9@gmail.com>
2018-05-14 20:41:02 -0700

    Move code from Http.Service.js to app.component.ts

--- pokemonAngular/src/app/app.component.ts
+++ pokemonAngular/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { HttpService } from './http.service';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service'; //(1) Import the HttpService 
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,45 @@ import { HttpService } from './http.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Pokemon API';
-  constructor(private _httpService: HttpService){}
+
+  constructor(private _httpService: HttpService){}; // (2) Make HttpService as an attribute in the class.
+  // ngOnInit will run when the component is initialized, after the constructor method.
+  ngOnInit(){
+    this.getPokemonFromService();
+  }
+  getPokemonFromService(){
+    var arr = [];
+ 
+    // First API Call
+    let bulbasaur = this._httpService.getPokemon();
+    bulbasaur.subscribe(data => {
+
+      let pokemonName = data['name'];
+      for (const i of data['abilities']) {
+          arr.push(i.ability.name);
+      }
+      // console.log(arr);
+      console.log(`${pokemonName.toUpperCase()}'s abilities are ${arr[0]} and ${arr[1]}.`); 
+      
+      // Second API Call
+      var nCount=0;
+      let all_pokemon = this._httpService.getAbility();
+      all_pokemon.subscribe(sData => {
+        for (const i of sData['results']) {
+          // console.log(arr, " VS " ,i.name);
+          // [1, 2, 3].includes(2); 
+          if (arr.includes(i.name)){
+            nCount += 1;
+          }
+        }
+        console.log(`${nCount} Pokemon share my favorite Pokemon's abilities.`);
+      });
+     
+    });
+
+
+  }  
+
 }
--- pokemonAngular/src/app/http.service.ts
+++ pokemonAngular/src/app/http.service.ts
@@ -1,45 +1,47 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';  
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
   constructor(private _http: HttpClient){
-    this.getPokemon();
-  }
-
-
+    // this.getPokemon();
+  } 
   
+  // 1st function to get obserable
   getPokemon(){
-    var arr = [];
-    // First API Call
-    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/12');
-    bulbasaur.subscribe(data => {
+    return this._http.get('https://pokeapi.co/api/v2/pokemon/12');
+    // var arr = [];
+    // // First API Call
+    // let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/12');
+    // bulbasaur.subscribe(data => {
 
-      let pokemonName = data['name'];
-      for (const i of data['abilities']) {
-          arr.push(i.ability.name);
-      }
-      console.log(arr);
-      console.log(`${pokemonName}'s abilities are ${arr[0]} and ${arr[1]}.`); 
-      
-      // Second API Call
-      var nCount=0;
-      let all_pokemon = this._http.get('https://pokeapi.co/api/v2/ability');
-      all_pokemon.subscribe(sData => {
-        for (const i of sData['results']) {
-          // console.log(arr, " VS " ,i.name);
-          // [1, 2, 3].includes(2); 
-          if (arr.includes(i.name)){
-            nCount += 1;
-          }
-        }
-        console.log(nCount, " Pokemon share my favorite Pokemon's abilities.");
-      });
-     
-    });
+    //   let pokemonName = data['name'];
+    //   for (const i of data['abilities']) {
+    //       arr.push(i.ability.name);
+    //   }
+    //   console.log(arr);
+    //   console.log(`${pokemonName}'s abilities are ${arr[0]} and ${arr[1]}.`); 
 
+    //   // Second API Call
+    //   var nCount=0;
+    //   let all_pokemon = this._http.get('https://pokeapi.co/api/v2/ability');
+    //   all_pokemon.subscribe(sData => {
+    //     for (const i of sData['results']) {
+    //       // console.log(arr, " VS " ,i.name);
+    //       // [1, 2, 3].includes(2); 
+    //       if (arr.includes(i.name)){
+    //         nCount += 1;
+    //       }
+    //     }
+    //     console.log(nCount, " Pokemon share my favorite Pokemon's abilities.");
+    //   });
+    // });
+  }
+  // 2nd function to get obserable
+  getAbility(){
+    return this._http.get('https://pokeapi.co/api/v2/ability');
   }
 
 }
